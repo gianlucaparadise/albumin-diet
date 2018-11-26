5f65611a0c0e490546166b7dcbf686d808c9715e
gianlucaparadise <gianlucaparadise@gmail.com>
2018-11-26 21:50:31 +0100

    removed boilerplate code

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -2,7 +2,6 @@
 
 import async from "async";
 import request from "request";
-import graph from "fbgraph";
 import { Response, Request, NextFunction } from "express";
 
 
@@ -15,19 +14,3 @@ export let getApi = (req: Request, res: Response) => {
     title: "API Examples"
   });
 };
-
-/**
- * GET /api/facebook
- * Facebook API example.
- */
-// export let getFacebook = (req: Request, res: Response, next: NextFunction) => {
-//   const token = req.user.tokens.find((token: any) => token.kind === "facebook");
-//   graph.setAccessToken(token.accessToken);
-//   graph.get(`${req.user.facebook}?fields=id,name,email,first_name,last_name,gender,link,locale,timezone`, (err: Error, results: graph.FacebookUser) => {
-//     if (err) { return next(err); }
-//     res.render("api/facebook", {
-//       title: "Facebook API",
-//       profile: results
-//     });
-//   });
-// };
